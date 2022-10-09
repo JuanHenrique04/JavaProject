aa103e8c41880db1cf481e0a19a9fd1b741c76b8
Juan Henrique <juanmisidoro@gmail.com>
2022-10-09 12:10:32 -0300

    Atualização de código

---
+++ TestClasse01/src/testclasse01/TestClasse01.java
@@ -0,0 +1,13 @@
+
+package testclasse01;
+
+public class TestClasse01 {
+
+    public static void main(String[] args) {
+        
+        Pessoa ze = new Pessoa();
+        ze.exibe();
+        
+    }
+    
+}
